f4376a1594792f691ccdfd849bde104d9b2a973b
ashlyochwada <ashly@carble.co>
2025-01-29 16:58:13 +0300

    prepared django project for deployment on render

--- ardhi_webgis/settings.py
+++ ardhi_webgis/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +26,9 @@ SECRET_KEY = 'django-insecure-9z999-nenw$j^de!8ofji!)ah=2v+cc0mgn@)l4mux@g(1sg0&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ardhi_backend.onrender.com']
+
 
 
 # Application definition
@@ -88,16 +91,28 @@ WSGI_APPLICATION = 'ardhi_webgis.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'ardhi_db',
+#         'USER': 'ardhi_user',
+#         'PASSWORD': 'ochwuma',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ardhi_db',
-        'USER': 'ardhi_user',
-        'PASSWORD': 'ochwuma',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
+
+# import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
 
 
 
