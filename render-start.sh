1d06d75de09f21becdf3d69641b74489c2450e39
ashlyochwada <ashly@carble.co>
2025-01-29 17:16:32 +0300

    Make start script executable

--- render-start.sh
+++ render-start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python manage.py migrate
 python manage.py collectstatic --noinput
-gunicorn ardhi_backend.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn ardhi_webgis.wsgi:application --bind 0.0.0.0:$PORT
